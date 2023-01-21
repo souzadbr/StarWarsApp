00fef10fca2100b91231e74a849cfc368c894f7a
Debora Rodrigues <deboral.p@hotmail.com>
2023-01-20 21:34:20 -0300

    Criar tela para receber inf de pessoas

--- StarWarsProjectApp/People/PeopleTableViewCell.swift
+++ StarWarsProjectApp/People/PeopleTableViewCell.swift
@@ -5,4 +5,41 @@
 //  Created by Debora Rodrigues  on 20/01/23.
 //
 
-import Foundation
+import UIKit
+
+ class PeopleTableViewCell: UITableViewCell {
+    
+    
+    //propriedade nameLAbel que é responsavel por mostrar o titulo no topo de cada célula.
+    let personLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = UIColor(cgColor: .init(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+        label.translatesAutoresizingMaskIntoConstraints = false // ativa o layout automático
+        return label
+    } ()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .clear
+        
+        self.contentView.addSubview(personLabel)
+        
+        applyContraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func applyContraints() {
+        
+        personLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 40).isActive = true
+        personLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 30).isActive = true
+        
+    }
+    
+    func updateCell(with people: Person) {
+        personLabel.text = "Person: \(people.name)"
+    }
+}
--- StarWarsProjectApp/People/PeopleViewController.swift
+++ StarWarsProjectApp/People/PeopleViewController.swift
@@ -5,4 +5,89 @@
 //  Created by Debora Rodrigues  on 20/01/23.
 //
 
-import Foundation
+import UIKit
+
+class PeopleViewController: UIViewController {
+    
+    let network = NetworkManager()
+    
+    var people: [Person]? = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.peopleTableView.reloadData()
+                print ("Carregando")
+            }
+        }
+    }
+    
+    let peopleTableView = UITableView() // view
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(peopleTableView)
+        view.backgroundColor = UIColor(cgColor: .init(red: 1, green: 1, blue: 1, alpha: 1))
+        peopleTableView.backgroundColor = .clear
+        //Faz parte do Segundo passo:
+        //Informar ao TableView de contatos onde seus métodos de protocolo de fonte de dados são implementados, neste caso, contactsViewController e em outras palavras, self.
+        peopleTableView.dataSource = self
+        peopleTableView.delegate = self
+        //contactsTableView não sabe sobre esta célula. Vamos corrigi-lo registrando-o usando a string do identificador.
+        // Acesse ViewDidLoad()no contactsViewControllere registre a célula no tableView.
+        peopleTableView.register(PeopleTableViewCell.self, forCellReuseIdentifier: "peopleCell")
+        setUpNavigation()
+        applyContraints()
+    }
+    
+    func applyContraints() {
+        //1. primeiro Passo.
+        //habilite o Auto Layout em contactsTableView definindo translatesAutoresizingMaskIntoConstraints como false.
+        peopleTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Defina o topAnchor de contactsTableView igual ao topAnchor da visualização principal.
+        //Isso garantirá que o contactsTableView permaneça na parte superior da visualização principal.
+        peopleTableView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+        //Vamos adicionar o código para as âncoras esquerda, direita e inferior semelhante ao código topAnchor.
+        peopleTableView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        peopleTableView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        peopleTableView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+    }
+    
+    func setUpNavigation() {
+        navigationItem.title = "People"
+        self.navigationController?.navigationBar.barTintColor = UIColor(cgColor: .init(red: 1, green: 1, blue: 1, alpha: 1))
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(cgColor: .init(red: 0, green: 0, blue: 0, alpha: 1))]
+    }
+    
+    func getDataFilm() {
+        network.fetchPeople { (people) in
+            self.people = people
+        }
+    }
+}
+
+extension PeopleViewController: UITableViewDataSource {
+    
+    //2 passo entrar em conformidade com protocolo UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Retorna o numero de contatos presentes no array trasendo numero de linhas por sessão.
+        return people?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //faz o resuso de celula formatada em contactCell retornando uma celula formatada.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath) as! PeopleTableViewCell
+        cell.updateCell(with: people?[indexPath.row] ?? Person(name: "Erro"))
+        return cell
+    }
+}
+
+extension PeopleViewController: UITableViewDelegate {
+    
+    //Funcao usada para dar altura na célula
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
+
