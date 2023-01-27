42b720d1afeb4b901c7c9e9c908ed93bb1720474
Debora Rodrigues <deboral.p@hotmail.com>
2023-01-27 08:11:38 -0300

    Montar layout e chamada de servico para species

--- StarWarsProjectApp/Species/SpeciesTableViewCell.swift
+++ StarWarsProjectApp/Species/SpeciesTableViewCell.swift
@@ -9,15 +9,52 @@ import UIKit
 
 class SpeciesTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = UIColor(cgColor: .init(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+        label.translatesAutoresizingMaskIntoConstraints = false // ativa o layout automático
+        return label
+    } ()
+    
+    let languageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = UIColor(cgColor: .init(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))
+        label.translatesAutoresizingMaskIntoConstraints = false // ativa o layout automático
+        return label
+    } ()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .clear
+        
+        self.contentView.addSubview(nameLabel)
+        self.contentView.addSubview(languageLabel)
+        
+        applyContraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func applyContraints() {
+        
+        //nameLabel
+        nameLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 15).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 70).isActive = true
+        
+        //classificationLabel
+        languageLabel.topAnchor.constraint(equalTo: self.nameLabel.topAnchor, constant: 50).isActive = true
+        languageLabel.leadingAnchor.constraint(equalTo: self.nameLabel.leadingAnchor, constant: 30).isActive = true
+        languageLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+    }
+    
+    func updateCell(with specie: Specie) {
+        nameLabel.text = "Name: \(specie.name)"
+        languageLabel.text = "Language: \(specie.language)"
     }
 
 }
